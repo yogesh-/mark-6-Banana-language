04d3a8f825163ad8f5c90d0b765ae24659a6475c
Yogesh Gupta <yogesh.gupta38@gmail.com>
2021-08-12 00:00:11 +0530

    taking user input and console

---
+++ app.js
@@ -0,0 +1,13 @@
+var btnTranslate = document.querySelector("#btn-translate");
+
+var txtInput = document.querySelector("#txt-input");
+console.log(txtInput);
+
+
+
+function clickHandler(){
+        console.log("Clicked");
+        console.log("input", txtInput.value);
+    };
+
+btnTranslate.addEventListener("click",clickHandler);
