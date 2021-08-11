ca9edb1a52bc205337f84ba7d7b6298af257fb2d
Yogesh Gupta <yogesh.gupta38@gmail.com>
2021-08-12 00:45:00 +0530

    div

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 var btnTranslate = document.querySelector("#btn-translate");
-
 var txtInput = document.querySelector("#txt-input");
-console.log(txtInput);
+var divBox = document.querySelector("output");
 
 
 
 function clickHandler(){
-        console.log("Clicked");
-        console.log("input", txtInput.value);
+        // divBox.innerText = "ssdscsd"+ txtInput.value; 
+        
     };
 
 btnTranslate.addEventListener("click",clickHandler);
