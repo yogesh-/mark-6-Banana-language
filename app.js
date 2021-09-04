381abeb9564a213a5a81c73445e489b8f26e3c28
Yogesh Gupta <yogesh.gupta38@gmail.com>
2021-09-04 21:34:12 +0530

    done

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
-var divBox = document.querySelector("output");
+var divBox = document.querySelector("#output");
 
+// var liveUrl = "	https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+var liveUrl = "https://api.funtranslations.com/translate/minion.json"
 
+function constructUrl (text){
+    return liveUrl + "?" + "text=" + text
+}
+
+function errorHandler(error){
+    console.log("error occured",error)
+    alert("Error occured, please try after sometime")
+}
 
 function clickHandler(){
-        // divBox.innerText = "ssdscsd"+ txtInput.value; 
-        
+        var inputText = txtInput.value;
+        fetch(constructUrl(inputText))
+        .then(response => response.json())
+        .then(json =>
+            {var transalatedText = json.contents.translated;
+            divBox.innerText = transalatedText;
+            })
+        .catch(errorHandler) 
     };
-
 btnTranslate.addEventListener("click",clickHandler);
